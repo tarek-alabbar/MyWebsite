fa9a3f4cb9dfedb608147c760472da3b8055da6a
Tarek <alabbar.tarek@gmail.com>
2022-03-24 21:57:26 +0000

    set routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,9 @@
-console.log("Hello world from server");
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.port || 5000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () => console.log(`Server Started on port ${port}`))
