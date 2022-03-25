cd9cfe5306de3cf742da2c364835e3913c2a70ee
Tarek <alabbar.tarek@gmail.com>
2022-03-25 01:33:34 +0000

    CRUD functionality

--- Backend/controllers/itemController.js
+++ Backend/controllers/itemController.js
@@ -1,34 +1,64 @@
 const asyncHandler = require('express-async-handler') 
 
+const Item = require('../models/itemModel')
+
 // @desc    Get items
 // @route   GET /api/items
 // @access  Private
 const getItems = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
-        res.status(400)
-        throw new Error('Please add a text field')
-    }
+    const items = await Item.find()
+
+    res.status(200).json(items)
 })
 
 // @desc    Set item
 // @route   POST /api/items
 // @access  Private
 const setItem = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Set item'})
+    if (!req.body.text) {
+        throw new Error('Please add a text field')
+    }
+
+    const item = await Item.create ({
+        text: req.body.text,
+    })
+
+    res.status(200).json(item)
 })
 
 // @desc    Update item
 // @route   UPDATE /api/items/:id
 // @access  Private
 const updateItem = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update item ${req.params.id}`})
+    const item = await Item.findById(req.params.id)
+
+    if (!item) {
+        res.status(400)
+        throw new Error('Item not found.')
+    }
+
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedItem)
 })
 
 // @desc    Delete item
 // @route   DELETE /api/items/:id
 // @access  Private
 const deleteItem = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete item ${req.params.id}`})
+    const item = await Item.findById(req.params.id)
+
+    if(!item){
+        res.status(400)
+        throw new Error('Item not found.')
+    }
+
+    await item.remove()
+
+
+    res.status(200).json( {id: req.params.id})
 })
 
 module.exports = {
