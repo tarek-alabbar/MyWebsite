377edf258dbaf2b5f3efeeb0c7d3f0f372ab9336
Tarek <alabbar.tarek@gmail.com>
2022-03-25 00:47:01 +0000

    async handler

--- Backend/controllers/itemController.js
+++ Backend/controllers/itemController.js
@@ -1,33 +1,36 @@
+const asyncHandler = require('express-async-handler') 
+
 // @desc    Get items
 // @route   GET /api/items
 // @access  Private
-const getItems = (req, res) => {
+const getItems = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
-}
+})
 
 // @desc    Set item
 // @route   POST /api/items
 // @access  Private
-const setItem = (req, res) => {
+const setItem = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Set item'})
-}
+})
 
 // @desc    Update item
 // @route   UPDATE /api/items/:id
 // @access  Private
-const updateItem = (req, res) => {
+const updateItem = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update item ${req.params.id}`})
-}
+})
 
 // @desc    Delete item
 // @route   DELETE /api/items/:id
 // @access  Private
-const deleteItem = (req, res) => {
+const deleteItem = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete item ${req.params.id}`})
-}
+})
+
 module.exports = {
     getItems,
     setItem,
