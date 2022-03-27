7b91425128e5277c10926e94c942e4468b53eed5
Tarek <alabbar.tarek@gmail.com>
2022-03-27 00:40:49 +0000

    Register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/dashboard'
+import Login from './pages/login'
+import Register from './pages/register'
+import Header from './components/Header'
+
 function App() {
   return (
-    <div>
-      <h1>My Website</h1>
+    <>
+    <Router>
+    <div className='container'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
     </div>
+    </Router>
+    </>
 
   )
 }
